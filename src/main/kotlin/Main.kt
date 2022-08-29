b5809394d1336ecb62aa2b4e8a6ddbbd1efcdf82
Elena Nefedova <elenanefedova@gmail.com>
2022-08-29 23:13:34 +0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,7 @@
+fun main(args: Array<String>) {
+    val amount: Int = 100010 // Ну просто число - какое попало
+    val tax: Int = amount * 35 / 100 // По идее будет целая часть числа
+    println("Money transfer comission is $tax")
+
+
+}
